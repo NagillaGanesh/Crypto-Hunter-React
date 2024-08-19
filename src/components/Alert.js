69a00c9602de8cb5d564d478484890d6e76fffca
NagillaGanesh <143380666+NagillaGanesh@users.noreply.github.com>
2024-08-19 10:15:01 +0530

    Add files via upload

---
+++ src/components/Alert.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { CryptoState } from '../CryptoContext'
+import { Snackbar } from '@material-ui/core';
+import MuiAlert from "@material-ui/lab/Alert";
+
+const Alert = () => {
+
+    const { alert, setAlert } = CryptoState();
+
+    const handleCloseAlert = (event, reason) => {
+        if (reason === "clickaway") {
+          return;
+        }
+    
+        setAlert({ open: false });
+      };
+
+  return (
+    <div>
+          <Snackbar
+            open={alert.open}
+            autoHideDuration={3000}
+            onClose={handleCloseAlert} 
+          >
+              <MuiAlert
+                    onClose={handleCloseAlert}
+                    elevation={10}
+                    variant="filled"
+                    severity={alert.type}
+                >
+                    
+                {alert.message}
+
+                </MuiAlert>
+              
+      </Snackbar>
+    </div>
+  )
+}
+
+export default Alert
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOYbVbDpRsYqGNC6UZ4iOW1oQAra66yaI",
+    authDomain: "crypto-hunter-654ed.firebaseapp.com",
+    projectId: "crypto-hunter-654ed",
+    storageBucket: "crypto-hunter-654ed.appspot.com",
+    messagingSenderId: "1061020623663",
+    appId: "1:1061020623663:web:f9d58ce725e31abdadf7da",
+    measurementId: "G-8Q7EQPZZ55"
+};
+  
+export default firebaseConfig;
